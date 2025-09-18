5d2e17f1934b87e813137ce0ff97114e5968ed56
javier Garzon <javi.garzon@hotmail.com>
2025-09-18 09:43:47 +0000

    n

---
+++ math.py
@@ -0,0 +1,48 @@
+num1 = 2.5
+num2 = 7
+num3 = 4
+num4 = 1.5
+
+# promedio
+promedio = (num1 + num2 + num3 + num4) / 4
+print('promedio:', promedio)
+
+# porcentaje
+porcentaje = (num1 / num2) * 100
+print('porcentaje:', porcentaje)
+
+# resta
+print('resta:', num2 - num1)
+
+# elevando
+print('elevando', num1 ** num4)
+
+# división entera
+print('division entera', num2 // num4)
+
+# resto
+print('resto:', num2 % num4)
+num1 = 2.5
+num2 = 7
+num3 = 4
+num4 = 1.5
+
+# promedio
+promedio = (num1 + num2 + num3 + num4) / 4
+print('promedio:', promedio)
+
+# porcentaje
+porcentaje = (num1 / num2) * 100
+print('porcentaje:', porcentaje)
+
+# resta
+print('resta:', num2 - num1)
+
+# elevando
+print('elevando', num1 ** num4)
+
+# división entera
+print('division entera', num2 // num4)
+
+# resto
+print('resto:', num2 % num4)
