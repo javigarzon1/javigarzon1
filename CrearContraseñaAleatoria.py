0d740d6dc18992036aa528795a5223057d066947
javier Garzon <javi.garzon@hotmail.com>
2025-09-20 10:36:36 +0000

    f

---
+++ CrearContraseñaAleatoria.py
@@ -0,0 +1,27 @@
+import random
+#crear una funcion que nos devuelva contraseñas aleatorias de 8 caracteres
+def crear_contraseña_aleatoria(num):
+    chars = "abcdefghijhklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
+    num_entero = str(num)
+    num = int(num_entero[0])
+    c1 = num + 11
+    c2 = num
+    c3 = num - 2
+    c4 = num + 5
+    c5 = num + 32
+    c6 = num + 8
+    c7 = num - 13
+    c8 = num + 3
+
+    contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{chars[c4]}{chars[c5]}{chars[c6]}{chars[c7]}{chars[c8]}{num*2}"
+    return contraseña,num
+aleatorio = random.randint(0, 9)
+
+password,primer_numero = crear_contraseña_aleatoria(aleatorio)
+
+#mostrando los resultados obtenidos y los datos utilizados para obtenerlo
+print(f"Tu contraseña nueva de 8 caracteres es: {password}")
+print(f"El nùmero utilizado para crear la contraseña fue: {aleatorio}")
+
+
+
--- funciones.py
+++ funciones.py
@@ -1,16 +1,15 @@
 
 #creando una función para crear una contraseña aleatoria 
 def crear_contraseña_random(num):
-    chars = "abcdefghij"
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
     num_entero = str(num)
     num = int(num_entero[0])
-    c1 = num - 2
+    c1 = num + 23
     c2 = num
-    c3 = num - 5
+    c3 = num + 11
     contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{num*2}"
     return contraseña,num
     
-#desempaquetando la funciòn
 password,primer_numero = crear_contraseña_random(345)
 
 #mostrando los resultados obtenidos y los datos utilizados para obtenerlo
